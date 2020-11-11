5b107bf3642fb5da2fe2ca2c81b4e72e3dcb55d9
15davidallison <15david.allison@gmail.com>
2020-11-11 13:17:01 -0800

    Fixed addUser and addGroup functionality. dialogButton now supports StatType parameter

--- src/AdminControlPanel.java
+++ src/AdminControlPanel.java
@@ -1,7 +1,3 @@
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Set;
-
 import javax.swing.*;  
 import javax.swing.tree.*;
 import javax.swing.event.TreeSelectionEvent;
@@ -19,13 +15,9 @@ import java.awt.event.WindowEvent;
 
 public class AdminControlPanel {
 	private static AdminControlPanel pointer; 
-	
-	private static JFrame adminFrame;
-	private JPanel treePanel;
 	private DefaultMutableTreeNode currentNodeSelection;
-	
-	private static DefaultMutableTreeNode root;
-	private static UserTree userTree;
+	private DefaultMutableTreeNode root;
+	private UserTree userTree;
 	
 	public static AdminControlPanel getInstance() {
 		if (pointer == null) {
@@ -38,12 +30,11 @@ public class AdminControlPanel {
 		userTree = new UserTree();
 		userTree.fillWithDummyData();
 		root = userTree.getRoot();
-
 	}
 	
 	public void launchPanel() {
 		// set up admin frame
-		adminFrame = new JFrame("Admin Control Panel");
+		JFrame adminFrame = new JFrame("Admin Control Panel");
 		adminFrame.setSize(800,600);
 	    adminFrame.setLayout(new FlowLayout());
 	    adminFrame.addWindowListener(new WindowAdapter() {
@@ -57,17 +48,20 @@ public class AdminControlPanel {
 	    messageCenter.setEditable(false); 
 	    
 	    // set up tree panel
-	    treePanel = new JPanel();
+	    JPanel treePanel = new JPanel();
 	    treePanel.setLayout(new FlowLayout());
 	    
 	    JTree tree = userTree.getJTree();
+	    for (int i = 0; i < tree.getRowCount(); i++) {
+	    	   tree.expandRow(i);
+	    	}
 	    tree.setCellRenderer(new customTreeRenderer());
 	    tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
 	    tree.addTreeSelectionListener(new TreeSelectionListener() {
 	        public void valueChanged(TreeSelectionEvent e) {
 	        	DefaultMutableTreeNode current = (DefaultMutableTreeNode)tree.getLastSelectedPathComponent();
 	        	if (current != null) {
-	        		currentNodeSelection = (DefaultMutableTreeNode)tree.getLastSelectedPathComponent();
+	        		currentNodeSelection = current;
 	        	}
 	        	messageCenter.setText("");
 	        }
@@ -77,86 +71,69 @@ public class AdminControlPanel {
 	    treePanel.add(treeView);
 	    
 	    // Set up buttons
-//	    JTextArea userArea = new JTextArea(1,10);
-//	    JButton addUser = new JButton("Add User");
-//	    addUser.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				DefaultMutableTreeNode parentNode;
-//				UserGroup selection = groups.get(currentNodeSelection.toString());
-//				// if current selection is a group, add this user to the selected group
-//				if (selection != null) {
-//					parentNode = selection.getNode();
-//				} else { // default: "Root"
-//					parentNode = (DefaultMutableTreeNode)model.getRoot();
-//				}
-//				// if user does not exist, add user
-//				int status = addUser(userArea.getText());
-//				switch(status) {
-//					case 0: // user successfully added
-//						DefaultMutableTreeNode newUser = new DefaultMutableTreeNode(userArea.getText());
-//						users.get(userArea.getText()).setNode(newUser);
-//						parentNode.add(newUser);
-//						model.reload();
-//						messageCenter.setText("User successfully added!");
-//						break;
-//					case 1:
-//						messageCenter.setText("User already exists!");
-//						break;
-//					case 2:
-//						messageCenter.setText("Invalid Username!");
-//						break;
-//					default:
-//						messageCenter.setText("Unknown Error");
-//				}
-//				// reset for expected behavior next time
-//				userArea.setText("");
-//				currentNodeSelection = (DefaultMutableTreeNode)model.getRoot();
-//			}
-//	    });
-//	    
-//	    JTextArea groupArea = new JTextArea(1,10);
-//	    JButton addGroup = new JButton("Add Group");
-//	    addGroup.addActionListener(new ActionListener() {
-//	    	public void actionPerformed(ActionEvent e) {
-//	    		DefaultMutableTreeNode parentNode;
-//				UserGroup selection = groups.get(currentNodeSelection.toString());
-//				// if current selection is a group, add this user to the selected group
-//				if (selection != null) {
-//					parentNode = selection.getNode();
-//				} else { // default: "Root"
-//					parentNode = (DefaultMutableTreeNode)model.getRoot();
-//				}
-//				// if group does not exist, add group
-//				int status = addGroup(groupArea.getText());
-//				switch(status) {
-//					case 0: // group successfully added
-//						DefaultMutableTreeNode newGroup = new DefaultMutableTreeNode(groupArea.getText());
-//						groups.get(groupArea.getText()).setNode(newGroup);
-//						parentNode.add(newGroup);
-//						model.reload();
-//						messageCenter.setText("Group successfully added!");
-//						break;
-//					case 1:
-//						messageCenter.setText("Group already exists!");
-//						break;
-//					case 2:
-//						messageCenter.setText("Invalid Username!");
-//						break;
-//					default:
-//						messageCenter.setText("Unknown Error");
-//				}	
-//				// reset for expected behavior next time
-//				groupArea.setText("");
-//				currentNodeSelection = (DefaultMutableTreeNode)model.getRoot();
-//	    	}
-//	    });
+	    JTextArea userArea = new JTextArea(1,10);
+	    JButton addUser = new JButton("Add User");
+	    addUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultMutableTreeNode parentNode;
+				DefaultMutableTreeNode result;
+				if (userArea.getText().equals("")) {
+					messageCenter.setText("Invalid Username!");
+					return;
+				}
+				if (currentNodeSelection != null && 
+						currentNodeSelection.getUserObject() instanceof UserGroup) {
+					parentNode = currentNodeSelection;
+					result = userTree.addUser(userArea.getText(), currentNodeSelection);
+				} else {
+					result = userTree.addUser(userArea.getText());
+				} 
+				if (result != null) {
+					model.reload();
+					messageCenter.setText("User successfully added!");
+				} else {
+					messageCenter.setText("User already exists!");
+				}
+				userArea.setText("");
+				currentNodeSelection = (DefaultMutableTreeNode)model.getRoot();
+			}
+	    });
+	    
+	    JTextArea groupArea = new JTextArea(1,10);
+	    JButton addGroup = new JButton("Add Group");
+	    addGroup.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		DefaultMutableTreeNode parentNode;
+	    		DefaultMutableTreeNode result;
+				if (groupArea.getText().equals("")) {
+					messageCenter.setText("Invalid group name!");
+					return;
+				}
+				if (currentNodeSelection != null && 
+						currentNodeSelection.getUserObject() instanceof UserGroup) {
+					parentNode = currentNodeSelection;
+					result = userTree.addGroup(groupArea.getText(), currentNodeSelection);
+				} else {
+					result = userTree.addGroup(groupArea.getText());
+				} 
+				if (result != null) {
+					model.reload();
+					messageCenter.setText("Group successfully added!");
+				} else {
+					messageCenter.setText("Group already exists!");
+				}
+				groupArea.setText("");
+				currentNodeSelection = (DefaultMutableTreeNode)model.getRoot();
+		    }
+	    });
 	    
 	    JButton openUser = new JButton("Open User View");
 	    openUser.setPreferredSize(new Dimension(400, 30));
 	    openUser.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (currentNodeSelection.toString() != null) {
-					System.out.println(currentNodeSelection.toString());
+				if (currentNodeSelection != null && currentNodeSelection.toString() != null && 
+						currentNodeSelection.getUserObject() instanceof User) {
+					new UserView(currentNodeSelection, userTree);
 	    		} else {
 	    			messageCenter.setText("Please select a user.");
 	    		}
@@ -166,38 +143,38 @@ public class AdminControlPanel {
 	    // add all 
 	    adminFrame.add(treePanel);
 	    JPanel userPanel = new JPanel();
-//	    userPanel.add(userArea);
-//	    userPanel.add(addUser);
+	    userPanel.add(userArea);
+	    userPanel.add(addUser);
 	    adminFrame.add(userPanel);
 	    
 	    JPanel groupPanel = new JPanel();
-//	    groupPanel.add(groupArea);
-//	    groupPanel.add(addGroup);
+	    groupPanel.add(groupArea);
+	    groupPanel.add(addGroup);
 	    adminFrame.add(groupPanel);
 	    
 	    adminFrame.add(openUser);
 	    
 	    JPanel dialogPanel = new JPanel();
 	    dialogPanel.add(dialogButton(adminFrame, "Total Number of Users",
-	    		"Number of users: " + userTree.getNumUsers()));
+	    		"Number of users: ", new NumUsers(userTree)));
 	    dialogPanel.add(dialogButton(adminFrame, "Total Number of Groups",
-	    		"Number of groups: " + userTree.getNumGroups()));
+	    		"Number of groups: ", new NumGroups(userTree)));
 	    dialogPanel.add(dialogButton(adminFrame, "Total Number of Tweets",
-	    		"Number of Tweets: " + numTweets()));
+	    		"Number of Tweets: ", new NumTweets(userTree)));
 	    dialogPanel.add(dialogButton(adminFrame, "Positive Percentage of Tweets",
-	    		"Percentage of Tweets that are positive: " + percentGoodTweets()));
+	    		"Percentage of Tweets that are positive: ", new PercentPosTweets(userTree)));
 	    adminFrame.add(dialogPanel);
 	   
 	    adminFrame.add(messageCenter);
 	    adminFrame.setVisible(true); 
 	}
 	
-	public JButton dialogButton(JFrame owner, String title, String desc) {
+	public JButton dialogButton(JFrame owner, String title, String desc, StatType data) {
 		JButton button = new JButton(title);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JDialog d = new JDialog(owner, title);
-		    	JLabel l = new JLabel(desc);
+		    	JLabel l = new JLabel(desc + data.getData());
 		    	d.setLayout(new FlowLayout());
 		    	d.add(l);
 		    	d.setVisible(true);
@@ -206,16 +183,9 @@ public class AdminControlPanel {
 		return button;
 	}
 	
-	public int numTweets() {
-		return -1;
-	}
-	
-	public double percentGoodTweets() {
-		return .25;
-	}
-	
 	private static class customTreeRenderer extends DefaultTreeCellRenderer {
-		public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+		public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, 
+				boolean expanded, boolean leaf, int row, boolean hasFocus) {
 			super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
 			if (value instanceof DefaultMutableTreeNode) {
 				 DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
